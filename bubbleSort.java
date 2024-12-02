feadb7c49bf603cfc72243760fc01e91b9c29443
arshkaur9091 <arshkaur9091@gmail.com>
2024-12-02 15:55:17 +0530

    bubble sort

---
+++ bubbleSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scn.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + " elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        // Perform Bubble Sort
+        bubbleSort(arr);
+
+        // Print the sorted array
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    // Bubble Sort implementation
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        // Outer loop for each pass
+        for (int i = 0; i < n - 1; i++) {
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
